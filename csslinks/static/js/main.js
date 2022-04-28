7db6f0007ed6ea639e74bf63d759ce61c2484851
Moringa School <wangu.mwangi@student.moringaschool.com>
2022-04-28 17:37:19 +0300

    initial commit

---
+++ csslinks/static/js/main.js
@@ -0,0 +1,9 @@
+function red() {
+    document.getElementById("demo").style.color= "red";
+}
+function coral() {
+    document.getElementById("demo").style.color= "coral";
+}
+function white() {
+    document.getElementById("demo").style.color= "white";
+}
